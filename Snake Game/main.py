5099e783f9f8ff03e9820d82df54f8e82650b162
MG Vigneshwaran <90309038+kingsmanvicky@users.noreply.github.com>
2023-02-27 22:53:09 +0530

    Add files via upload

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,6 +1,8 @@
-from turtle import Turtle,Screen
-import time
+from turtle import Screen
 from snake import Snake
+from food import Food
+import time
+from scoreboard import ScoreBoard
 
 
 screen = Screen()
@@ -9,7 +11,12 @@ screen.bgcolor("black")
 screen.title("Nokia's- THE SNAKE")
 screen.tracer(0)
 
+
 snake = Snake()
+food = Food()
+score = ScoreBoard()
+
+
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
@@ -23,16 +30,23 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    #Collision with food:
+    if snake.head.distance(food) < 15:
+        food.refresh() 
+        score.increase_score()
+        snake.extend()
+    
+    #Detect collision with wall:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
+        game_is_on = False
+        score.game_over()
 
-
-
-
-
-
-
-
-
-
+    
+    #Detect head and tail collision:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 
 
 screen.exitonclick()
--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -22,6 +22,17 @@ class Snake:
             new_segment.goto(position)
             self.segments.append(new_segment)
 
+    def add_segment(self,position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
